5e4e29b21603b44769a190de46902fcda1604232
Khadar Mohamed Ahmed <qadarbot@gmail.com>
2025-05-25 19:14:22 +0300

    Book Mark the Chalange complated

---
+++ Book Mark/main.js
@@ -0,0 +1,67 @@
+const TitalEl = document.getElementById('title');
+const UrlEl = document.getElementById('url');
+const BookMarkFormEl = document.getElementById('bookmark-form');
+const BookMarkListEl = document.getElementById('bookmark-list');
+
+let bookmarks = JSON.parse(localStorage.getItem('bookmarks')) || [];
+
+BookMarkFormEl.addEventListener('submit', addBookmark);
+
+function addBookmark(e) {
+    e.preventDefault();
+
+    // get form values
+    const title = TitalEl.value.trim();
+    const url = UrlEl.value.trim();
+
+    if (!title || !url) {
+        Swal.fire({
+            icon: 'warning',
+            title: 'Oops...',
+            text: 'Please fill in both fields!',
+            confirmButtonColor: '#d33'
+        });
+        return;
+    }
+
+    bookmarks.push({
+        id: Date.now(),
+        title,
+        url,
+    });
+
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
+    updateBookmarkList();
+    BookMarkFormEl.reset();
+}
+function updateBookmarkList() {
+    BookMarkListEl.innerHTML = '';
+
+    bookmarks.forEach((bookmark) => {
+        const bookmarkEl = createBookmarkElement(bookmark);
+        BookMarkListEl.appendChild(bookmarkEl);
+    });
+}
+function createBookmarkElement(bookmark) {
+    const li = document.createElement('li');
+    li.classList.add('bookmark');
+
+    li.innerHTML = `
+        
+        <span>
+            <a href="${bookmark.url}" target="_blank">${bookmark.title}</a>
+            <button class="delete-btn" onclick="removeBookmark(${bookmark.id})">Remove</button>
+        </span>
+    `;
+
+    return li;
+}
+function removeBookmark(id) {
+    bookmarks = bookmarks.filter((bookmark) => bookmark.id !== id);
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    updateBookmarkList();
+}
+
+updateBookmarkList();
+
